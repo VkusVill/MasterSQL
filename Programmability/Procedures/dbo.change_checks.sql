ae4318d428b095feba482e516eca0a5e1f1ec998
VkusVill <godoffgoodood@gmail.com>
2019-12-23 22:06:52 +0300

    Making new SP

---
+++ Programmability/Procedures/dbo.change_checks.sql
@@ -0,0 +1,20 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:  	<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+--select * from jobs..jobs with(nolock) where job_name like '%change_checks%'
+-- =============================================
+
+create PROCEDURE [dbo].[change_checks] @id_job int
+AS
+BEGIN
+  SET NOCOUNT ON
+  
+  DECLARE
+    @getdate  datetime     = GETDATE(),
+    @job_name varchar(100) = com.dbo.Object_name_for_err(@@Procid, DB_ID())
+
+END
+GO
