f5f2b11b4f57f12e00e724d30003ffd027e6e20c
VkusVill <godoffgoodood@gmail.com>
2019-12-23 22:05:24 +0300

    New Uset

---
+++ Tables/dbo.TestTable.sql
@@ -0,0 +1,26 @@
+﻿CREATE TABLE [dbo].[TestTable] (
+  [col1] [int] NULL,
+  [col2] [numeric](10) NULL
+)
+ON [PRIMARY]
+GO
+
+SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER [Instead_Insert_TestTable]
+   ON  [dbo].[TestTable]
+   INSTEAD OF INSERT
+AS 
+BEGIN
+	SET NOCOUNT ON;
+	declare @maxID numeric = (select max(col2) from dbo.TestTable)
+    Insert Into dbo.TestTable (col1, col2)
+	Select i.col1, ROW_NUMBER() over (order by i.col1) + @maxID
+	From inserted i 
+END
+GO
---
+++ Tables/dbo.TestTable2.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[TestTable2] (
+  [col1] [int] NULL,
+  [col2] [numeric](10) NULL
+)
+ON [PRIMARY]
+GO
